9369dce9847cf5697afd0d5a861371b34721d22d
Nelson Londoño <nelsonlondonodev@gmail.com>
2025-09-03 20:52:30 +0200

    chore: Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ahorrapp/', // Add this line for GitHub Pages deployment
   plugins: [react(), tailwindcss()],
 })
